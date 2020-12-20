142c6a477440a7b5d468c19d2a006573e2c9f16b
suzuki-gunji <72844589+suzuki-gunji@users.noreply.github.com>
2020-12-20 14:56:10 +0900

    料理ジャンルの変更

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,11 +3,10 @@ class Genre < ActiveHash::Base
     { id: 1, genre: '和食' },
     { id: 2, genre: '麺類' },
     { id: 3, genre: '揚げ物' },
-    { id: 4, genre: '粉物' },
+    { id: 4, genre: '鍋' },
     { id: 5, genre: '中華' },
     { id: 6, genre: 'イタリアン' },
     { id: 7, genre: 'フレンチ' },
-    { id: 8, genre: '鍋' },
   ]
 
   include ActiveHash::Associations
