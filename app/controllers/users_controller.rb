cb6fca372ac4e67857e01169a414ff522bba9b7b
suzuki-gunji <gunji1102@gmail.com>
2021-03-09 20:49:56 +0900

    マイページの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(current_user.id)
     @nickname = @user.nickname
+    @cooks = Cook.order("created_at DESC")
   end
 end
